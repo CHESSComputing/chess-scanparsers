631c9fca7abbdefe6d75fe4aff6e5ab8ff8df080
keara-soloway <kls286@cornell.edu>
2024-05-06 14:28:39 -0400

    ci: add files required for pages deployment

---
+++ docs/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'chess-scanparsers'
+copyright = '2024 CHESSComputing'
+author = 'K. Soloway, R. Verberg'
+release = 'PACKAGE_VERSION'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'autodoc2',
+    'sphinxarg.ext'
+]
+autodoc2_packages = [
+    '../chess-scanparsers'
+]
+autodoc2_render_plugin = 'myst'
+myst_enable_extensions = ['fieldlist']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
+html_show_copyright = False
+html_theme = 'classic'
+html_sidebars = {
+    '**': ['globaltoc.html',
+           'localtoc.html',
+           'relations.html',
+           'searchbox.html']
+}
---
+++ docs/installation.md
@@ -0,0 +1,44 @@
+# Installation
+
+
+<!---
+## Using `pip`
+`chess-scanparser` is available over PyPI. Run
+```bash
+pip install chess-scanparser
+```
+to install.
+
+## Using `conda`
+CHAP is available over conda-forge. Run
+```bash
+conda install -c conda-forge chess-scanparser
+```
+to install.
+-->
+
+## From source
+1. Clone the source repository from github.
+   ```bash
+   git clone https://github.com/CHESSComputing/chess-scanparser.git
+   cd chess-scanparser
+   ```
+1. Set a valid version number. In `setup.py`, replace:
+   ```{literalinclude} /../setup.py
+   :language: python
+   :start-after: '[set version]'
+   :end-before: '[version set]'
+   ```
+   with
+   ```python
+   version = 'PACKAGE_VERSION'
+   ```
+1. Setup a local installation prefix for your version of python
+   ```bash
+   mkdir -p install/lib/python<yourpythonversion>/site-packages
+   export PYTHONPATH=$PWD/install/lib/python<yourpythonversion>/site-packages
+   ```
+1. Use the setup script to install the package
+   ```bash
+   python setup.py install --prefix=$PWD/install
+   ```
